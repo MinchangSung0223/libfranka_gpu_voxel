a9a92a3f168a7fb11fca44fa8233cb79007d2df4
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-05-17 13:51:06 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake . -D icl_core_DIR=/workspace/gpu-voxels/build/packages/icl_core/ -D gpu_voxels_DIR=/workspace/gpu-voxels/build/packages/gpu_voxels
+cmake . -D icl_core_DIR=~/workspace/gpu-voxels/build/packages/icl_core/ -D gpu_voxels_DIR=~/workspace/gpu-voxels/build/packages/gpu_voxels
