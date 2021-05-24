c60ae87e3543d48a7980b661e31c5e2781778642
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-05-24 11:22:49 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -r CMakeCache.txt
 cmake . -D icl_core_DIR=~/workspace/gpu-voxels/build/packages/icl_core/ -D gpu_voxels_DIR=~/workspace/gpu-voxels/build/packages/gpu_voxels
 make -j16
